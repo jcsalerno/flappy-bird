09be4bdd7ebd07ccec1a149c6eab1d12d3f5ecf2
Julio Cesar <contato.jcsalerno@gmail.com>
2023-12-06 19:21:08 -0300

    Finalizando a função principal - Moivmentação dos objetos

--- flappybird.py
+++ flappybird.py
@@ -191,3 +191,29 @@ def main():
                 if evento.key == pygame.K_SPACE:
                     for passaro in passaros:
                         passaro.pular()    
+        
+        for passaro in passaros:
+            passaro.mover()
+        chao.mover()
+
+        adicionar_cano = False
+        remover_canos = []
+        for cano in canos:
+            for i, passaro in enumerate(passaros):
+                if cano.colidir(passaro):
+                    passaros.pop(i)
+                if not cano.passou and passaro.x cano.x:
+                    cano.passou = True
+            cano.mover()
+            if cano.x + cano.CANO_TOPO.get_width() < 0:
+                remover_canos.append(cano) 
+
+        if adicionar_cano:        
+            pontos += 1
+            canos.append(Cano(600))
+        for cano in remover_canos:
+            canos.remove(cano)
+
+        for i, passaro in enumerate(passaros):
+            if (passaro.y + passaro.imagem.get_height()) > chao.y or passaro.y < 0:
+                passaros.pop(i)                       
